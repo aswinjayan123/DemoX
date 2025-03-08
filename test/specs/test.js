325e9f27b997e5b3c228dc9f1cf37a0497369faa
Aswin Jayan <aswinjayan010@gmail.com>
2025-03-08 09:32:11 +0530

    added-two-teststeps

---
+++ test/specs/test.js
@@ -0,0 +1,17 @@
+
+import Launchpage from "../pageobjects/Launchpage";
+import ResultsPage from "../pageobjects/ResultsPage";
+import deleteCartPage from "../pageobjects/deleteCartPage";
+describe('Product Adding to the cart', function () {
+    it('Check Url is launched', async () => {
+        await Launchpage.LaunchUrl();
+        await expect(await Launchpage.$logo()).toBeDisplayed();
+    })
+    
+
+    it('Check Electronics is selected from the dropdown', async () => {
+        await Launchpage.selectdropdown();
+        const selectedValue = await Launchpage.$selectoption().getValue();
+        expect(selectedValue).toContain('search-alias=electronics');
+    });
+})
